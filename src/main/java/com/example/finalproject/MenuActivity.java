cfdc1f98e3787afba7a2b5051736a7bc920ac22a
vnukovandrey <andrix.ork@gmail.com>
2021-05-22 21:02:10 +0300

    ывы

--- src/main/java/com/example/finalproject/MenuActivity.java
+++ src/main/java/com/example/finalproject/MenuActivity.java
@@ -1,8 +1,6 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.annotation.SuppressLint;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
