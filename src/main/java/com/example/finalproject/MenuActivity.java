ee29239f0e6a4f5a4d4b2a09385724df921ac37d
vnukovandrey <andrix.ork@gmail.com>
2021-05-22 22:23:27 +0300

    Initial commit

--- src/main/java/com/example/finalproject/MenuActivity.java
+++ src/main/java/com/example/finalproject/MenuActivity.java
@@ -1,20 +1,21 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 
 public class MenuActivity extends AppCompatActivity {
     Button bt1;
     EditText txt1, txt2;
     TextView txt3;
-
 
 
     //TODO процесс шифрования
@@ -25,37 +26,54 @@ public class MenuActivity extends AppCompatActivity {
 
         Toast.makeText(this, "activity_menu", Toast.LENGTH_SHORT).show();
         bt1 = findViewById(R.id.button1);
-        txt1 =  findViewById(R.id.editText1);
-        txt2 =  findViewById(R.id.editText2);
+        txt1 = findViewById(R.id.editText1);
+        txt2 = findViewById(R.id.editText2);
         txt3 = findViewById(R.id.textView2);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txt1.getText().toString().equals("") || txt2.getText().toString().equals("")) {
+                    txt3.setText("Строка пустая, попробуйте еще раз.");
 
-    }
+                } else {
+                    Spinner spinner = findViewById(R.id.spinner);
+                    String selected = spinner.getSelectedItem().toString();
+                    StringBuilder string = new StringBuilder();
+                    String s1 = txt1.getText().toString();
+                    String s2 = txt2.getText().toString();
 
-    public void onClick(View view) {
+                    int x1 = s1.trim().length();
+                    int x2 = Integer.parseInt(s2.trim());
 
-        if (txt1.getText().toString().equals("") || txt2.getText().toString().equals("")) {
-            txt3.setText("Строка пустая, попробуйте еще раз.");
+switch (selected) {
+    case "Шифр Цезаря":
+        String alf = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        for (int i = 0; i < x1; i++) {
 
-        } else {
-            StringBuilder string = new StringBuilder();
-            String s1 = txt1.getText().toString();
-            String s2 = txt2.getText().toString();
-            int x1 = s1.length();
-            int x2 =  Integer.parseInt(s2.trim());
-            for (int i = 0; i < x1; i++) {
+            for (int j = 0; j < 33; j++) {
+                if (s1.charAt(i) == alf.charAt(j)) {
 
-                for (char j = 'а'; j <= 'я'; j++) {
-                    if (s1.charAt(i) ==  j) {
-                        string.append((j+x2)%33);
-                        txt3.setText(j);
-                    }
-
+                    string.append(alf.charAt((j + x2) % 33));
                 }
-
             }
-            txt3.setText(string);
         }
+        txt3.setText(string); break;
+    case "Литорея":
+        char letter;
+        for(int i = 0; i<x1; i++)
+            letter = s1.charAt(i);
+            switch (letter){
+                case 'б': string.append("щ"); break;
+                case 'в': string.append("ш"); break;
+            }
+        break;
+}}
+            }
+        });
     }
+
+
 }
+
 
 
