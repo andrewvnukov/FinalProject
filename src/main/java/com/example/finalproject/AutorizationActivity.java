654e83bd0f6e574ebdeeeb9437ecb3b056857c7a
vnukovandrey <andrix.ork@gmail.com>
2021-06-02 10:30:32 +0300

    Initial commit

--- src/main/java/com/example/finalproject/AutorizationActivity.java
+++ src/main/java/com/example/finalproject/AutorizationActivity.java
@@ -1,17 +1,59 @@
 package com.example.finalproject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 //TODO Авторизация
 
-public class AutorizationActivity extends AppCompatActivity {
+public class AutorizationActivity extends RegisterActivity {
+    SharedPreferences saver;
+    EditText txt1, txt2;
+    TextView txt3;
+    Button bt1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autorization);
+        saver = getSharedPreferences(APP_PREFERENCES_NAME, Context.MODE_PRIVATE);
+        txt1 = findViewById(R.id.name);
+        txt2 = findViewById(R.id.password);
+        txt3 = findViewById(R.id.textView7);
+        bt1 = findViewById(R.id.button);
+        txt3.setVisibility(View.GONE);
+        bt1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (saver.contains(txt1.toString()) && saver.contains(txt2.toString())) {
+                    txt1.setVisibility(View.GONE);
+                    txt2.setVisibility(View.GONE);
+                    bt1.setVisibility(View.GONE);
+                    txt3.setText("Вы успешно вошли!");
+                    txt3.setVisibility(View.VISIBLE);
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Intent i;
+                    i = new Intent(AutorizationActivity.this, MenuActivity.class);
+                    startActivity(i);
+                    onStop();
+
+                } else {Toast.makeText(getBaseContext(), "Неправильно указан пароль или имя пользователя", Toast.LENGTH_LONG).show();}
+            }
+        });
     }
 }
